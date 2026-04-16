3f94cb6c9554b0096a52df847eaf2d58d1c1ab7c
Denuka Gamage <dca.gamage10@gmail.com>
2026-04-16 12:33:09 +0530

    Create DB Connection using MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+//password = XTUWIKqhjjCvbivd
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+//MongoDB querySrv ECONNREFUSED issue fix
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"])
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res) => {
+    res.send("It is Working....");
+})
+
+mongoose.connect("mongodb+srv://admin:XTUWIKqhjjCvbivd@cluster0.awbtuer.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(()=> {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
