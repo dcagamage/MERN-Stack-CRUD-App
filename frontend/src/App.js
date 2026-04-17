776131b2f9f5cbcea9549d48d83f073e9bb4b109
Denuka Gamage <dca.gamage10@gmail.com>
2026-04-17 10:44:50 +0530

    Set up the Frontend environment using React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from "./Components/Home/Home";
+import User from './Components/AddUser/User';
+import Users from './Components/UserDetails/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <React.Fragment>
+        <Routes>
+          <Route path="/" element={ <Home/> }/>
+          <Route path="/mainhome" element={ <Home/> }/>
+          <Route path="/adduser" element={ <User/> }/>
+          <Route path="/userdetails" element={ <Users/> }/>
+        </Routes>
+      </React.Fragment>
     </div>
   );
 }
